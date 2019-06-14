92c98d751e01178fba22917841d7368f839fb22a
Jimmy Huang <jhuang@bastiansolutions.com>
2019-06-14 15:41:51 -0400

    did some stuff

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+import UserList from './UserList';
+
+const App = () => {
+    const [resource, setResource] = useState('posts');
+
+    return( 
+        <div>
+            <UserList />
+            <div>
+                <button onClick={() => setResource('posts')}>Posts</button>
+                <button onClick={() => setResource('todos')}>Todos</button>
+            </div>
+                <ResourceList resource={resource}/>
+        </div>
+    );
+}
+
+/*
+import React from 'react';
+
+class App extends React.Component {
+    state = { resource: 'posts' }
+
+    render() {
+        return( 
+            <div>
+                <div>
+                    <button onClick={() => this.setState({ resource: 'posts'})}>Posts</button>
+                    <button onClick={() => this.setState({ resource: 'todos'})}>Todos</button>
+                </div>
+                    {this.state.resource}
+            </div>
+
+        );
+    }
+}
+*/
+export default App;
